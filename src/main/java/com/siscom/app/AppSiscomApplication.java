6093d025c88a6e8116fd9e158db92084514a637b
Nicholas Maestrello Agiz <rainickmr@gmail.com>
2018-03-14 01:25:11 -0300

    Projeto

---
+++ src/main/java/com/siscom/app/AppSiscomApplication.java
@@ -0,0 +1,12 @@
+package com.siscom.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSiscomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSiscomApplication.class, args);
+	}
+}
---
+++ src/test/java/com/siscom/appSiscom/AppSiscomApplicationTests.java
@@ -0,0 +1,16 @@
+package com.siscom.appSiscom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppSiscomApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
