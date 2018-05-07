2c35e4ff94dd72bb393420fa9455db701c7ddd90
Nicholas Maestrello Agiz <rainickmr@gmail.com>
2018-05-07 20:29:28 -0300

    correção para cross origin de requesição

--- src/main/java/com/siscom/siscom/controller/AlunoController.java
+++ src/main/java/com/siscom/siscom/controller/AlunoController.java
@@ -3,7 +3,6 @@ package com.siscom.siscom.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,8 +27,7 @@ public class AlunoController {
 	public String inserir(@RequestBody AlunoDTO aluno) {
 		return alunoService.inserirAluno(aluno);
 	}
-	
-	@CrossOrigin
+
 	@RequestMapping(method = RequestMethod.PUT, path="/api/aluno")
 	public String alterar(@RequestBody AlunoDTO aluno) {
 		return alunoService.alterarAluno(aluno);
